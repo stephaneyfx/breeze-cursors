2a5c6dfb50cdd13196982233974dda3d278048ac
Stephane Raux <stephaneyfx@gmail.com>
2023-11-19 16:02:29 -0600

    Add flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Breeze cursors";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils/main";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      breeze-cursors = pkgs.stdenv.mkDerivation {
+        pname = "breeze-cursors";
+        version = self.lastModifiedDate;
+        src = self;
+        installPhase = ''
+          mkdir -p $out/share/icons
+          cp -r Breeze $out/share/icons
+          cp -r Breeze_Light $out/share/icons
+        '';
+        meta = {
+          description = "Breeze cursors";
+          homepage = "https://github.com/stephaneyfx/breeze-cursors";
+          license = pkgs.lib.licenses.lgpl3Only;
+          platforms = pkgs.lib.platforms.all;
+        };
+      };
+    in {
+      packages = {
+        inherit breeze-cursors;
+        default = breeze-cursors;
+      };
+    }
+  );
+}
