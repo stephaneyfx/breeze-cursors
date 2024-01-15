a63274999da7e97ed4c944eb11fc7879daea870c
Stephane Raux <stephaneyfx@gmail.com>
2024-01-14 18:45:14 -0600

    Add default overlay

--- flake.nix
+++ flake.nix
@@ -6,30 +6,33 @@
     flake-utils.url = "github:numtide/flake-utils/main";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils }:
     let
-      pkgs = nixpkgs.legacyPackages.${system};
-      breeze-cursors = pkgs.stdenv.mkDerivation {
-        pname = "breeze-cursors";
-        version = self.lastModifiedDate;
-        src = self;
-        installPhase = ''
-          mkdir -p $out/share/icons
-          cp -r Breeze $out/share/icons
-          cp -r Breeze_Light $out/share/icons
-        '';
-        meta = {
-          description = "Breeze cursors";
-          homepage = "https://github.com/stephaneyfx/breeze-cursors";
-          license = pkgs.lib.licenses.lgpl3Only;
-          platforms = pkgs.lib.platforms.all;
+      packagesFor = pkgs:
+        let
+          breeze-cursors = pkgs.stdenv.mkDerivation {
+            pname = "breeze-cursors";
+            version = self.lastModifiedDate;
+            src = self;
+            installPhase = ''
+              mkdir -p $out/share/icons
+              cp -r Breeze $out/share/icons
+              cp -r Breeze_Light $out/share/icons
+            '';
+            meta = {
+              description = "Breeze cursors";
+              homepage = "https://github.com/stephaneyfx/breeze-cursors";
+              license = pkgs.lib.licenses.lgpl3Only;
+              platforms = pkgs.lib.platforms.all;
+            };
+          };
+        in {
+          inherit breeze-cursors;
+          default = breeze-cursors;
         };
-      };
     in {
-      packages = {
-        inherit breeze-cursors;
-        default = breeze-cursors;
-      };
-    }
-  );
+      overlays.default = final: prev: packagesFor final;
+    } // flake-utils.lib.eachDefaultSystem (system: {
+      packages = packagesFor nixpkgs.legacyPackages.${system};
+    });
 }
